f8e5a5ce0e2a0156fd388b3be8027790a7d0d45c
Bryan Jennings <bryanjenningz@gmail.com>
2018-02-11 16:41:24 -0800

    add splash screen text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react';
 import './App.css';
 
+const range = (n: number): number[] => Array.from({ length: n }, (_, i) => i);
+
+type State = { slideIndex: number };
+
 class App extends React.Component {
+  state: State = { slideIndex: 0 };
+
   render() {
+    const { slideIndex } = this.state;
     return (
       <div>
-        Duolingo
+        <div>duolingo</div>
+        <div>Free language education for the world.</div>
+        <div className="dots">
+          {range(4).map(i => (
+            <div
+              key={i}
+              className={`dot ${i === slideIndex ? 'big' : 'small'}`}
+            >
+              .
+            </div>
+          ))}
+        </div>
+        <div>GET STARTED</div>
+        <div>I ALREADY HAVE AN ACCOUNT</div>
       </div>
     );
   }
