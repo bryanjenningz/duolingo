051b96164b94b3ec959d1d8b637466322e14970a
Bryan Jennings <bryanjenningz@gmail.com>
2018-02-11 17:13:07 -0800

    style layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,31 @@ class App extends React.Component {
   render() {
     const { slideIndex } = this.state;
     return (
-      <div>
-        <div>duolingo</div>
-        <div>Free language education for the world.</div>
-        <div className="dots">
-          {range(4).map(i => (
-            <div
-              key={i}
-              className={`dot ${i === slideIndex ? 'big' : 'small'}`}
-            >
-              .
+      <div className="bg-blue text-white full-screen">
+        <div className="main-container d-flex flex-col vh-100">
+          <div className="flex-2 d-flex flex-col">
+            <div className="flex-1 d-flex flex-col justify-end align-center">
+              duolingo
             </div>
-          ))}
+            <div className="flex-1 d-flex flex-col space-around align-center">
+              <div>Free language education for the world.</div>
+              <div className="d-flex">
+                {range(4).map(i => (
+                  <div
+                    key={i}
+                    className={`dot ${i === slideIndex ? 'big' : 'small'}`}
+                  >
+                    .
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="flex-1 d-flex flex-col space-around align-center">
+            <div>GET STARTED</div>
+            <div>I ALREADY HAVE AN ACCOUNT</div>
+          </div>
         </div>
-        <div>GET STARTED</div>
-        <div>I ALREADY HAVE AN ACCOUNT</div>
       </div>
     );
   }
